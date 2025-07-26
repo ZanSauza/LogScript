89212d14d63f006ffcbc11f9b62f7bf21bb71867
Zan <zanfilipp259@gmail.com>
2025-07-26 12:52:53 +0300

    2

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     argument_parser.add_argument("--file", dest="file", type=str)
     argument_parser.add_argument("--report average", dest="report_average", type=str)
     argument_parser.add_argument("--date",dest="date", type=str)
+    argument_parser.add_argument("--table", dest="table", type=str)
 
     args = argument_parser.parse_args()
 
@@ -19,9 +20,16 @@ def main():
     parser.read_file()
     parse_handler = parser.parsing_items("url", parser.handlers)
     parse_user_agent_handler = parser.parsing_items("http_user_agent", parser.user_agent)
-    parser.parse_file()
-    table1 = TableCreator(parser.parse_info, arg_report)
-    table1.create_table()
+    if not arg_date:
+        parser.parse_file()
+        table1 = TableCreator(parser.parse_info, arg_report)
+        table1.create_table()
+    if arg_date:
+        date = f'{arg_date[0]+arg_date[1]+arg_date[2]+arg_date[3]+arg_date[4]+arg_date[8]+arg_date[9]+arg_date[7]+arg_date[5]+arg_date[6]}'
+        parser.parse_file(date)
+        table2 = TableCreator(parser.parse_info, arg_report)
+        table2.create_table()
+
 
     # print(parser.parse_info)
 
--- utils/table_creator.py
+++ utils/table_creator.py
@@ -9,8 +9,19 @@ class TableCreator:
         table = []
         if self.argument == "average":
             for elems in self.info:
+                print(elems.items())
                 if not table:
                     table.append(['handler', 'total', 'avg_response_time'])
+                    table.append([[key for key in elems.keys()][-1],
+                                  [value[0]['total'] for value in elems.values()][-1],
+                                  [value[1]['avg_response_time'] for value in elems.values()][-1]])
+                else:
+                    table.append([[key for key in elems.keys()][-1],
+                                  [value[0]['total'] for value in elems.values()][-1],
+                                  [value[1]['avg_response_time'] for value in elems.values()][-1]])
 
+
+
+        print(table)
         table = tabulate(table, headers="firstrow", tablefmt="grid")
         print(table)
